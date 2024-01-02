704c0fba2e8e904f30cef4f2c51d4596c79e1366
stribus <paladinotoldo@gmail.com>
2024-01-01 23:01:17 -0300

    Classe decriptografia para criptografar e descriptografar

---
+++ Crypto/MyCrypto.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+
+namespace Crypto
+{
+    /// <summary>
+    /// classe de criptografia para "brincar"
+    /// baseado na versão php https://github.com/stribus/MyCripty/blob/master/class.cripto.php   
+    /// </summary> 
+    public class MyCrypto
+    {
+        private int key;
+        private string salt;
+
+        /// <summary>
+        /// inicializa com os valores
+        /// </summary>
+        /// <param name="key">um numero primo usado para os calculos    </param>
+        /// <param name="salt">salt </param>
+        public MyCrypto(int key, string salt)
+        {
+            this.key = key;
+            this.salt = salt;
+        }
+
+        public string Encrypt(string text)
+        {
+            text = text + salt;
+            string base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(text));
+            if(base64.Length > int.MaxValue/this.key)
+            {
+                throw new Exception("Texto muito grande");
+            }
+            string result = "";
+            int leng = base64.Length+1;
+            for (int i = 1; i < leng; i++)
+            {
+                int ni = i * this.key;
+                if (ni >= leng)
+                {
+                    ni = ni % leng;
+                }
+                if (ni == 0)
+                {
+                    ni = i;
+                }
+                result += base64[ni -1];
+            }
+            return result;
+        }
+
+        public string Decrypt(string text)
+        {
+            string result = "";
+            int leng = text.Length+1;
+            var n = this.key;
+
+            for(int i = 1; i < leng; i++)
+            {
+                int ni = i * n;
+                if (ni % leng == 1)
+                {
+                    n = i;
+                    break;
+                }
+            }
+
+            for (int i = 1; i < leng; i++)
+            {
+                int ni = i * n;
+                if (ni >= leng)
+                {
+                    ni = ni % leng;
+                }
+                if (ni == 0)
+                {
+                    ni = i;
+                }
+                result += text[ni - 1];
+            }
+
+            byte[] base64 = Convert.FromBase64String(result);
+            result = Encoding.UTF8.GetString(base64);
+            result = result.Substring(0, result.Length - salt.Length);
+            return result;
+        }
+    }
+}
---
+++ consoleTeste/Program.cs
@@ -0,0 +1,16 @@
+﻿using Crypto;
+
+Console.WriteLine("Hello, World!");
+
+MyCrypto myCripty = new MyCrypto(7, "3a9873f36b140fde0441d929a43e0bfae41f2d5c");
+
+string texto = "texto para criptografar";
+string criptografado = myCripty.Encrypt(texto);
+string descriptografado = myCripty.Decrypt(criptografado);
+
+Console.WriteLine($"Texto: {texto}");
+Console.WriteLine($"Criptografado: {criptografado}");
+Console.WriteLine($"Descriptografado: {descriptografado}");
+
+
+
